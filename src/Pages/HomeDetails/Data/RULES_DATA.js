363ccc6310723010fb2566e145e982ebf094e0c8
eunsonny <67362026+eunsonny@users.noreply.github.com>
2020-11-04 15:30:22 +0900

    Add: Home Details Page 레이아웃 추가
    * 초기세팅
    * Add: homeDetails Page 추가 레이아웃
    * conflict 해결

---
+++ src/Pages/HomeDetails/Data/RULES_DATA.js
@@ -0,0 +1,31 @@
+const RULES_DATA = [
+  {
+    icon: "https://www.flaticon.com/svg/static/icons/svg/2948/2948025.svg",
+    title: "집 전체",
+    desc: "아파트 전체를 단독으로 사용하시게 됩니다",
+  },
+  {
+    icon: "https://www.flaticon.com/svg/static/icons/svg/1825/1825540.svg",
+    title: "청결 강화",
+    desc:
+      "에어비앤비의 강화된 5단계 청소 절차를 준수하겠다고 동의한 호스트입니다",
+  },
+  {
+    icon: "https://www.flaticon.com/svg/static/icons/svg/1828/1828427.svg",
+    title: "셀프 체크인",
+    desc: "열쇠 보관함을 이용해 체크인하세요",
+  },
+  {
+    icon: "https://www.flaticon.com/svg/static/icons/svg/876/876754.svg",
+    title: "12월 6일 3:00 PM까지 무료 취소 가능",
+    desc:
+      "11월 17일 12:00 PM 전에 예약을 취소하면 총 숙박 요금의 50% 및 서비스 수수료 전액이 환불됩니다.",
+  },
+  {
+    icon: "https://www.flaticon.com/svg/static/icons/svg/709/709957.svg",
+    title: "숙소 이용규칙",
+    desc: "반려동물 동반이나 흡연이 금지되는 숙소입니다.",
+  },
+];
+
+export default RULES_DATA;
