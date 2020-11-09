899b6770b04388dcab24d2725babf7a94c9d90b8
lemongucci <developerdawnnn@gmail.com>
2020-11-09 10:39:48 +0900

    Add: Filter 용 모달 컴포넌트 파일 생성 - 백엔드 사이드와 어느 정도 수준으로 필터 기능을 구현할 지 상의한 후 필터 기능 구현할 예정 - Google-map-react 라이브러리 사용하여 우측 지도 컴포넌트 생성 후 백엔드 사이드와 기능 구현 수준 상의할 예정

---
+++ src/Pages/HomeList/Components/Filters/TypeOfPlace.js
@@ -0,0 +1,238 @@
+import React from "react";
+import styled from "styled-components";
+
+const WrapperTOP = styled.div`
+  & > div {
+    border-radius: 13px;
+    min-width: 350px;
+    max-width: 375px;
+    height: auto;
+    background-color: white;
+    opacity: 1;
+    z-index: 300;
+    margin: 0 auto;
+
+    & > section:first-child {
+      max-width: 375px;
+      max-height: 290px;
+      border-bottom: 1px solid rgb(235, 235, 235);
+      padding: 20px;
+      overflow-y: scroll;
+      overflow-x: hidden;
+      background-color: white;
+    }
+
+    & > section:nth-child(2) {
+      max-width: 375px;
+      height: 65px;
+      padding: 12px 14px;
+      display: flex;
+      align-items: center;
+
+      & > div {
+        position: relative;
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+        & > button:first-child {
+          background-color: transparent;
+          border: none;
+          border-bottom: 1.5px solid rgb(235, 235, 235);
+          outline: none;
+          cursor: pointer;
+          color: rgb(235, 235, 235);
+          font-size: 16px;
+          font-weight: 600;
+          height: 20px;
+          padding: 0;
+          &:active {
+            opacity: 0.6;
+          }
+          &:hover {
+            color: rgb(0, 0, 0);
+          }
+        }
+        & > button:nth-child(2) {
+          background-color: rgb(34, 34, 34);
+          border-radius: 8.5px;
+          border: none;
+          outline: none;
+          color: white;
+          font-size: 14px;
+          font-weight: 600;
+          padding: 8px 16px;
+          cursor: pointer;
+          &:active {
+            transform: scale(0.97);
+          }
+        }
+      }
+    }
+  }
+`;
+
+const CheckBox4TOP = styled.div`
+  & > h2 {
+    font-size: 22px;
+    font-weight: 600;
+    color: rgb(34, 34, 34);
+    margin-bottom: 28px;
+  }
+  & > div {
+    & > label {
+      display: inline-block;
+      width: 320px;
+      height: auto;
+      margin: 12px 0;
+
+      & > div {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        & > .checkbox {
+          width: 24px;
+          height: 24px;
+          border-radius: 4px;
+          border: 1px solid rgb(185, 185, 185);
+          padding: 0;
+          margin: 0;
+          &:hover {
+            border: 1px solid rgb(34, 34, 34);
+          }
+
+          & > span {
+            & > input {
+              opacity: 0;
+              margin: 0;
+              cursor: pointer;
+              height: 00px;
+              width: 0px;
+            }
+          }
+        }
+
+        & > div {
+          padding: 0 12px;
+          width: 300px;
+          & > span {
+            color: rgb(34, 34, 34);
+            font-size: 16px;
+            line-height: 20px;
+            font-weight: 400;
+            word-break: keep-all;
+          }
+          & > p {
+            word-break: keep-all;
+            font-weight: 400;
+            line-height: 18px;
+            font-size: 14px;
+            color: rgb(34, 34, 34);
+          }
+        }
+      }
+    }
+  }
+  & > button {
+    background-color: transparent;
+    border: none;
+    border-bottom: 1.5px solid rgb(34, 34, 34);
+    outline: none;
+    cursor: pointer;
+    color: rgb(34, 34, 34);
+    font-size: 16px;
+    height: 20px;
+    padding: 0;
+    margin: 5px 0;
+    &:active {
+      opacity: 0.6;
+    }
+    &:hover {
+      color: rgb(0, 0, 0);
+    }
+  }
+`;
+
+const TypeOfPlace = () => {
+  return (
+    <WrapperTOP>
+      <div>
+        <section>
+          <CheckBox4TOP>
+            <div>
+              <label>
+                <div>
+                  <div className="checkbox">
+                    <span>
+                      <input type="checkbox"></input>
+                    </span>
+                  </div>
+                  <div>
+                    <span>집 전체</span>
+                    <p>집 전체를 단독으로 사용합니다</p>
+                  </div>
+                </div>
+              </label>
+              <label>
+                <div>
+                  <div className="checkbox">
+                    <span>
+                      <input type="checkbox"></input>
+                    </span>
+                  </div>
+                  <div>
+                    <span>개인실</span>
+                    <p>
+                      침실은 단독으로 쓰고, 이외의 공간은 호스트나 다른 게스트와
+                      함께 이용할 수도 있습니다.
+                    </p>
+                  </div>
+                </div>
+              </label>
+              <label>
+                <div>
+                  <div className="checkbox">
+                    <span>
+                      <input type="checkbox"></input>
+                    </span>
+                  </div>
+                  <div>
+                    <span>호텔 객실</span>
+                    <p>
+                      부티크 호텔, 호스텔 등의 개인실이나 다인실을 이용합니다
+                    </p>
+                  </div>
+                </div>
+              </label>
+              <label>
+                <div>
+                  <div className="checkbox">
+                    <span>
+                      <input type="checkbox"></input>
+                    </span>
+                  </div>
+                  <div>
+                    <span>다인실</span>
+                    <p>
+                      사적 공간 없이, 침실이나 욕실 등을 호스트나 다른 게스트와
+                      함께 이용합니다
+                    </p>
+                  </div>
+                </div>
+              </label>
+            </div>
+          </CheckBox4TOP>
+        </section>
+        <section>
+          <div>
+            <button>지우기</button>
+            <button>저장</button>
+          </div>
+        </section>
+      </div>
+    </WrapperTOP>
+  );
+};
+
+export default TypeOfPlace;
--- src/Pages/HomeList/HomeList.js
+++ src/Pages/HomeList/HomeList.js
@@ -1,7 +1,262 @@
 import React from "react";
+import styled, { keyframes } from "styled-components";
 
+import Card from "./Components/Card";
+import GoogleMap from "./Components/GoogleMap";
+
+import MoreFilters from "./Components/Filters/MoreFilters";
+import RoomsAndBeds from "./Components/Filters/RoomsAndBeds";
+import Price from "./Components/Filters/Price";
+import CancellationFlexibility from "./Components/Filters/CancellationFlexibility";
+import TypeOfPlace from "./Components/Filters/TypeOfPlace";
+import InstantBook from "./Components/Filters/InstantBook";
+
+//스타일
+const WrapperHL = styled.div`
+  width: 98vw;
+  height: 100%;
+  position: relative;
+  /* position: fixed; */
+  & > section:first-child {
+    position: fixed;
+    top: 0;
+    right: 0;
+    display: block;
+    width: 98vw;
+    margin-bottom: 80px;
+    //네비게이션바 여기 조절
+  }
+
+  & > section:nth-child(2) {
+    position: relative;
+    top: 80px;
+    left: 0;
+    width: 98vw;
+    display: flex;
+    flex-direction: column;
+
+    & > div:first-child {
+      display: flex;
+      width: 98vw;
+      //리스트
+      & > div:first-child {
+        width: 840px;
+        height: auto;
+        //필터
+        & > div:first-child {
+          width: 840px;
+          height: 180px;
+          padding: 0 24px;
+          padding-top: 50px;
+          margin-bottom: 10px;
+        }
+        & > div:nth-child(2) {
+          padding: 0 24px;
+        }
+      }
+
+      & > div:nth-child(2).map {
+        width: calc(98vw - 840px);
+        height: 100vh;
+        position: sticky;
+        top: 80px;
+        right: 0;
+      }
+    }
+
+    & > div:nth-child(2) {
+      display: block;
+      width: 98vw;
+      height: 50px;
+    }
+  }
+
+  & > section:nth-child(3) {
+    display: block;
+    width: 98vw;
+    height: 300px;
+  }
+`;
+
+const Filter = styled.div`
+  & > p {
+    font-size: 14px;
+    line-height: 18px;
+    margin-bottom: 8px;
+  }
+
+  & > h1 {
+    font-size: 30px;
+    line-height: 36px;
+    font-weight: 800;
+  }
+  & > div.btns {
+    display: flex;
+    margin: 20px 0;
+    & > div {
+      & > .btnFilter {
+        margin-right: 5px;
+        position: relative;
+        & > .modalFilter {
+          position: absolute;
+          top: 50px;
+          left: 0;
+          border-radius: 13px;
+          box-shadow: rgba(0, 0, 0, 0.15) 0px 10px 37px;
+          height: auto;
+          z-index: 300;
+        }
+      }
+    }
+  }
+`;
+
+const Button = styled.button`
+  margin-right: 5px;
+  cursor: pointer;
+  padding: 8px 16px;
+  height: 36px;
+  border-radius: 30px;
+  border: 1px solid rgb(176, 176, 176);
+  background-color: white;
+  &:hover {
+    border: 1px solid black;
+  }
+  &:focus {
+    outline: none;
+    border: 1px solid rgb(34, 34, 34);
+
+    box-shadow: 1px -1px rgb(34, 34, 34), -0.7px -1px rgb(34, 34, 34),
+      1px 1px rgb(34, 34, 34), -0.7px 1px rgb(34, 34, 34);
+  }
+  & > span {
+    font-size: 14px;
+    color: rgb(34, 34, 34);
+    font-stretch: 100%;
+    font-weight: 400;
+    line-height: 18px;
+  }
+`;
+
+const MD = () => {
+  return <MoreFilters />;
+};
+
+//함수
 const HomeList = () => {
-  return <div>HomeList</div>;
+  return (
+    <>
+      {/* 필터리스트 */}
+      {/* <MoreFilters /> */}
+      <WrapperHL>
+        <section>
+          <div>haha</div>
+        </section>
+
+        <section>
+          <div>
+            <div>
+              <div>
+                <Filter>
+                  <p>
+                    300개 이상의 숙소 · 11월 20일 - 12월 14일 (±3일) · 게스트
+                    3명
+                  </p>
+                  <h1>제주시의 숙소</h1>
+                  <div className="btns">
+                    <div>
+                      <div className="btnFilter">
+                        <Button>
+                          <span>유연한 환불 정책</span>
+                        </Button>
+                        {/* <div className="modalFilter">
+                          <CancellationFlexibility />
+                        </div> */}
+                      </div>
+                    </div>
+
+                    <div>
+                      <div className="btnFilter">
+                        <Button>
+                          <span>숙소 유형</span>
+                        </Button>
+                      </div>
+                      <div className="modalFilter">
+                        <TypeOfPlace />
+                      </div>
+                    </div>
+
+                    <div>
+                      <div className="btnFilter">
+                        <Button>
+                          <span>요금</span>
+                        </Button>
+                      </div>
+                      {/* <div className="modalFilter">
+                        <Price />
+                      </div> */}
+                    </div>
+
+                    <div>
+                      <div className="btnFilter">
+                        <Button>
+                          <span>즉시 예약</span>
+                        </Button>
+                      </div>
+                      {/* <div className="modalFilter">
+                        <InstantBook />
+                      </div> */}
+                    </div>
+
+                    <div>
+                      <div className="btnFilter">
+                        <Button>
+                          <span>필터 추가하기</span>
+                        </Button>
+                      </div>
+                    </div>
+                  </div>
+                </Filter>
+              </div>
+
+              <div>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+              </div>
+            </div>
+
+            <div className="map">
+              <GoogleMap />
+            </div>
+          </div>
+
+          <div></div>
+        </section>
+
+        <section>
+          <div></div>
+        </section>
+      </WrapperHL>
+    </>
+  );
 };
 
 export default HomeList;
